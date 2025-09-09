270de93549cb472b8f036372a87071afcb46af8b
Gauri0710 <panwargauri5@gmail.com>
2025-09-09 19:11:01 +0530

    react basics

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent=React.createElement("div",{id:"parent"},
-  React.createElement("div",{id:"child"},
-  React.createElement("h1",{},"This is namaste react")
-));
-console.log(parent);
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); 
+//react element
+const Title = () => (<h1 className="head"tabIndex="5">
+  Namaste React using JSX
+  </h1>
+  );
+  
+  
+  //React component
+  //Class based component
+  //Functional component
+
+  /*const HeadingComponent=()=>{
+    return <h1 className="Heading">Functional COMPONENT</h1>
+  };*/
+  
+  const HeadingComponent =()=>(
+    <div id ="container">
+      <Title />
+    <h1 className ="heading>"> Functional Component</h1>
+    </div>
+  );
+    const root=ReactDOM.createRoot(document.getElementById("root"));
+  root.render(<Title/>);
+  root.render(<HeadingComponent />);
+      
+        
+      
