eaf969ee9946fff383089e9589413cf15ff9375a
Gauri0710 <panwargauri5@gmail.com>
2025-09-10 21:40:57 +0530

    navbar

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//react element
-const Title = () => (<h1 className="head"tabIndex="5">
-  Namaste React using JSX
-  </h1>
-  );
-  
-  
-  //React component
-  //Class based component
-  //Functional component
 
-  /*const HeadingComponent=()=>{
-    return <h1 className="Heading">Functional COMPONENT</h1>
-  };*/
-  
-  const HeadingComponent =()=>(
-    <div id ="container">
-      <Title />
-    <h1 className ="heading>"> Functional Component</h1>
+
+const Header=()=>{
+  return(
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://tse2.mm.bing.net/th/id/OIP.9hl54qFeHA_2o_PIo3JhswAAAA?rs=1&pid=ImgDetMain&o=7&rm=3" alt="logo"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+
+      </div>
     </div>
-  );
+  )
+}
+const AppLayout=()=>{
+  return(
+    <div className="app">
+      <Header />
+
+
+    </div>
+  )
+}
+
     const root=ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Title/>);
-  root.render(<HeadingComponent />);
+  
+  root.render(<AppLayout />);
       
         
       
